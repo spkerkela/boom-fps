6ef488af1256d6a60c027f5f0fdb6474a2951e00
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.fi>
2016-12-19 17:22:29 +0200

    move src around, compile all c files in src

---
+++ src/initialize.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "initialize.h"
+
+int other_func(char* in) {
+  printf("%s\n", in);
+  return 32;
+}
--- src/main.c
+++ src/main.c
@@ -2,10 +2,18 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <math.h>
+#include <SDL2/SDL.h>
+
+#include "initialize.h"
 
 const double CIRCLE = M_PI * 2;
 int main(int argc, char** argv) {
     printf("%s\n", "Hello, BOOM!");
+    printf("%s\n", argv[0]);
     printf("%f\n", CIRCLE);
+    uint32_t n = other_func("sd");
+    printf("%d\n", n);
     return 0;
 }
+
+
